396fcfed22f43a1645cdf19c99e3f0aad864bcd3
Omar Leal <omarenriquelealr@gmail.com>
2022-10-29 13:48:39 -0400

    first commit

---
+++ News-App-Searcher/News-App-Searcher/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  News-App-Searcher
+//
+//  Created by Omar Leal on 10/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "News"
+        view.backgroundColor = .systemBackground
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
